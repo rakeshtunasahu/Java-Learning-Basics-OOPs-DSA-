715eaa66b5d6e8aeb20c49d0882ba32a62852642
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-06-25 19:55:14 +0530

    added the binary search algorithm

--- Practice_basics_java/Basic_program/reverse.java
+++ Practice_basics_java/Basic_program/reverse.java
@@ -1,18 +1,18 @@
-package Practice_basics_java.Basic_program;
-import java.util.*;
-public interface reverse {
-     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the numbers: ");
-        int num=sc.nextInt();
-        int answer =0;
+// package Practice_basics_java.Basic_program;
+// import java.util.*;
+// public interface reverse {
+//      public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the numbers: ");
+//         int num=sc.nextInt();
+//         int answer =0;
     
-        while(num >0){
-             int a = num % 10;
-             answer = answer*10+num;
-             num/=10;
-        }
-        System.out.println(answer);
-        sc.close();
-    }
-}
+//         while(num >0){
+//              int a = num % 10;
+//              answer = answer*10+num;
+//              num/=10;
+//         }
+//         System.out.println(answer);
+//         sc.close();
+//     }
+// }
--- Search_sorting_algorithm/Binary_search/learning.java
+++ Search_sorting_algorithm/Binary_search/learning.java
@@ -1,5 +1,28 @@
 package Search_sorting_algorithm.Binary_search;
 
 public class learning {
-    
+    public static void main(String[] args) {
+         int [] arr={1,3,5,6,7,9,12,17,34,67,89,99};
+         int target =89;
+         int ans=binarySearch(arr, target);
+         System.out.println(target +" is found in "+ans+"th index");
+    }
+    static int binarySearch(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid=start + (end-start)/2;
+        
+            if(target<arr[mid]){
+                end=mid-1;
+            }else if (target > arr[mid]){
+                start=mid +1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+  
 }
