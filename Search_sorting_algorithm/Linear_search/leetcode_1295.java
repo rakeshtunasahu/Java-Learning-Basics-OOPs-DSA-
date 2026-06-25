159e698e933152bca8023ca2ce4af9d39858a664
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-06-25 18:02:45 +0530

    started the leetcode journey through linear search

---
+++ Search_sorting_algorithm/Linear_search/leetcode_1295.java
@@ -0,0 +1,42 @@
+package Search_sorting_algorithm.Linear_search;
+
+public class leetcode_1295 {
+    public static void  main(String[] args){
+        int [] nums={12,345,2,6,7896};
+        System.out.println(findNumbers(nums));
+    }
+    static int findNumbers(int[] nums) {
+        int count = 0;
+
+        for(int num:nums){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean even(int num){
+    int numberOfDigits = digits(num);
+
+    return numberOfDigits % 2 == 0;
+}
+    static int digits(int num){
+        if(num<0){
+            num=num*-1;
+        }
+        if(num==0){
+            return 1;
+        }
+
+        int count =0;
+            while(num>0){
+                count++;
+                num/=10;
+            }
+            return count;
+        }
+    } 
+
+
+
