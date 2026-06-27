16ef4793290a82997daeb2783f720022478577fc
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-06-27 20:46:09 +0530

    added the binary sear

--- Search_sorting_algorithm/Binary_search/ceiling_of_the_number.java
+++ Search_sorting_algorithm/Binary_search/ceiling_of_the_number.java
@@ -1,11 +1,12 @@
 package Search_sorting_algorithm.Binary_search;
 
-public class learning {
+public class ceiling_of_the_number {
     public static void main(String[] args) {
+        //ceiling of a numbers:getting the biggest numbers of the target number
          int [] arr={1,3,5,6,7,9,12,17,34,67,89,99};
          int target =98;
          int ans=binarySearch(arr, target);
-         System.out.println(target +" is found in "+ans+"th index");
+         System.out.println(ans+ " is the required number");
     }
     static int binarySearch(int[] arr,int target){
         int start=0;
@@ -22,7 +23,7 @@ public class learning {
                 return mid;
             }
         }
-        return -1;
+        return start;
     }
-  
+
 }
