66ed1494f44cb8d8a64861b443ffb865a9240cda
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-06-27 20:49:52 +0530

    added the binary search programs

---
+++ Search_sorting_algorithm/Binary_search/floor_of_the_number.java
@@ -0,0 +1,29 @@
+package Search_sorting_algorithm.Binary_search;
+
+public class floor_of_the_number {
+    public static void main(String[] args) {
+         //floor of a numbers:getting the smallest numbers of the target number
+         int [] arr={1,3,5,6,7,9,12,17,34,67,89,99};
+         int target =98;
+         int ans=binarySearch(arr, target);
+         System.out.println(ans+ " is the required number");
+    }
+    static int binarySearch(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid=start + (end-start)/2;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        
+            if(target<arr[mid]){
+                end=mid-1;
+            }else if (target > arr[mid]){
+                start=mid +1;
+            }else{
+                return mid;
+            }
+        }
+        return end;
+    }
+}
+
