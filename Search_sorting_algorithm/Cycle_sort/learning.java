77bca2b10ca99c12a27f4e3c791cb48171cdebe9
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-10 17:57:52 +0530

    added the cycle sort  problem

--- Search_sorting_algorithm/Cycle_sort/learning.java
+++ Search_sorting_algorithm/Cycle_sort/learning.java
@@ -1,5 +1,28 @@
 package Search_sorting_algorithm.Cycle_sort;
 
+import java.util.Arrays;
+
 public class learning {
-    
+    public static void main(String[] args) {
+        int[]arr={7,3,8,2,1,5,4,6};
+        cyclesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static  void cyclesort(int[] arr){
+        int i =0;
+        while(i<arr.length){
+            int correct =arr[i]-1;
+            if(arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+
+    }
+    static void swap(int [] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;        
+    }
 }
