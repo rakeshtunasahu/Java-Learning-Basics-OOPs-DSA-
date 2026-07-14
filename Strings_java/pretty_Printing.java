c5726b63cd731b727211c428ed44ef266bc62970
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-14 19:35:46 +0530

    added the releated programs

--- Strings_java/pretty_Printing.java
+++ Strings_java/pretty_Printing.java
@@ -1,5 +1,12 @@
 package Strings_java;
 
 public class pretty_Printing {
-    
+    public static void main(String[] args) {
+        float a=13.444552323331231f;
+        System.out.printf("formatted number s %.4f", a);
+        
+        System.out.printf("  pie value %.3f",Math.PI);
+
+        System.out.printf("   my name is %s and i very %s","rakesh","very cool");
+    }
 }
