e0070a9eaf16ea3706329c4635aa93949e69fdfa
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-14 20:59:11 +0530

    completed string builders

--- Strings_java/performance.java
+++ Strings_java/performance.java
@@ -1,5 +1,12 @@
 package Strings_java;
 
 public class performance {
-    
+    public static void main(String[] args) {
+        String series="";
+        for(int i =0;i<26;i++){
+            System.out.println((char)('a'+i));
+            // series +=ch;
+            System.out.println(series);
+        }
+    }
 }
