8a2973b3cc97bee508e26a81f61e7e09a2e5994c
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-19 01:29:22 +0530

    added the resursion questions

--- Recursion.java/learning_recursion.java
+++ Recursion.java/learning_recursion.java
@@ -1,5 +1,16 @@
 
 public class learning_recursion {
-    
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    static void print(int n) {
+        if (n == 5) {
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
 }
 
