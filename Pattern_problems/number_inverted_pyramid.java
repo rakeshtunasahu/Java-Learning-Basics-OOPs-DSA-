c0f6dc213cf6150d299ab458972bbe96f1905ed2
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-27 17:31:12 +0530

    pattern problems

--- Pattern_problems/number_inverted_pyramid.java
+++ Pattern_problems/number_inverted_pyramid.java
@@ -1,8 +1,9 @@
 package Pattern_problems;
+
 import java.util.Scanner;
 
-public class inverted_pyramid {
-    public static void main(String[] args){
+public class number_inverted_pyramid {
+      public static void main(String[] args){
         Scanner  sc = new Scanner(System.in);
     
         System.out.print("enter the rows: ");
@@ -11,10 +12,12 @@ public class inverted_pyramid {
 
         for(int i=n;i>=1;i--){
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(j+" ");
             }
             System.out.println();
         }
         sc.close();
     }
 }
+
+
