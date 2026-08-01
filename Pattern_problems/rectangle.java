31b70e866972ccf86801afd545d38c83c5a09ff3
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-08-01 22:00:02 +0530

    edited the probems

--- Pattern_problems/rectangle.java
+++ Pattern_problems/rectangle.java
@@ -1,21 +1,24 @@
 package Pattern_problems;
-import java.util.*;
-public class rectangle {
-    public static void main(String[] args){
-        Scanner  sc = new Scanner(System.in);
-    
-        System.out.print("enter the rows: ");
-        int n= sc.nextInt();
-        System.out.print("enter the columns: ");
-        int m= sc.nextInt();
 
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=5;j++){
+import java.util.*;
+
+public class rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the rows: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter the columns: ");
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {      // Use n
+            for (int j = 1; j <= m; j++) {  // Use m
                 System.out.print("*");
             }
-            System.out.println(" ");
+            System.out.println();           // Move to next line
         }
+
         sc.close();
     }
-    
 }
